d70a86afda2d11f3338248cf2567081b2722c186
EL-Nabil-Hakimi <140151842+EL-Nabil-Hakimi@users.noreply.github.com>
2023-07-21 09:12:47 +0100

    Add files via upload

---
+++ js.js
@@ -0,0 +1,36 @@
+function myFunction() {
+    var x = document.getElementById("nv1");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+
+function Message(){
+  var x = document.getElementById("msgE");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+
+function ovrirM() {
+  var x = document.getElementById("modaleshow");
+ 
+    x.style.display = "block";
+ 
+}
+
+function FirmeM() {
+  var x = document.getElementById("modaleshow");
+ 
+    x.style.display = "none";
+ 
+}
+
+
+
